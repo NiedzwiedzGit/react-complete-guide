e7c27794a852f0f80f44dfe47bf09c84dbdb0f2d
Igor Skliarsky <shkliarskiyigor@gmail.com>
2020-03-26 20:18:42 +0100

    Using Radium for Media Queries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
@@ -82,14 +82,16 @@ class App extends Component {
       classes.push('bold');
     }
     return (
-      <div className="App" >
-        <h1>hi,im a react appppp </h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle persons</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App" >
+          <h1>hi,im a react appppp </h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button
+            style={style}
+            onClick={this.togglePersonsHandler}>Toggle persons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
     //   return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'qqqqqqqqqqq'));
 
