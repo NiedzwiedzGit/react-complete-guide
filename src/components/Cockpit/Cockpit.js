6ac6087209f48bb73553739c98bb38da071d04bb
Igor Skliarsky <shkliarskiyigor@gmail.com>
2020-03-30 21:03:34 +0200

    Using Refs

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.css';
 const cockpit = (props) => {
+    const toggleBtnRef = useRef(null);
+
     useEffect(() => {
         console.log('[Cockpit.js] useEffect ');
-        setTimeout(() => {
-            alert('Saved data to cloud');
-        }, 1000);
+        // setTimeout(() => {
+        //     alert('Saved data to cloud');
+        // }, 1000);
+        toggleBtnRef.current.click();
         return () => {
             console.log('[Cockpit.js] cleanup work in useEfect ');
         };
@@ -26,8 +29,11 @@ const cockpit = (props) => {
         <div className={classes.Cockpit}>
             <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
-            <button className={btnClass}
-                onClick={props.clicked}>Toggle persons
+            <button
+                ref={toggleBtnRef}
+                className={btnClass}
+                onClick={props.clicked}>
+                Toggle persons
           </button>
         </div>
     );
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,25 +1,46 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 //import './Person.css';
-//import Aux from '../../../hoc/Auxiliary';
-//import classes from './Person.css';
+import Aux from '../../../hoc/Auxiliary';
+import withClass from '../../../hoc/withClass';
+import classes from './Person.css';
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
     render() {
         console.log('[Person.js] render...');
         return (
-            <Fragment>
+            <Aux>
                 {/* // <div className="Person" style={style}> */}
                 {/* <div className={classes.Person} > */}
                 < p onClick={this.props.click} > i'm {this.props.name} person and i'm {this.props.age} years old</p >
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input
+                    //ref={(inputEl) => { this.inputElement = inputEl }}
+                    ref={this.inputElementRef}
+                    type="text"
+                    onChange={this.props.changed}
+                    value={this.props.name}
+                />
                 {/* </div > */}
-            </Fragment>
-        )
+            </Aux>
+        );
     }
 }
+Person.PropTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 
-
-export default Person;
+export default withClass(Person, classes.Person);
 
